cefa9d5e0042d6d343a8cbf50212c57c12663b50
Arpita Naval <navalarpita@gmail.com>
2021-05-26 09:43:15 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req,res){
+
+var today = new Date();
+var currentDay = today.getDay();
+var day = "";
+if (currentDay === 6 || currentDay ===0){
+day = "weekend";
+}
+else{
+day = "weekday";
+}
+
+});
+
+app.listen(3000, function() {
+  console.log(`Server running on port 3000: http://localhost:${3000} `);
+});
