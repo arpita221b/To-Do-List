2f5173c1781f75d538b8fbfb52ef5a0e787c19b8
Arpita Naval <navalarpita@gmail.com>
2021-05-26 09:54:22 +0530

    Add Day

--- app.js
+++ app.js
@@ -10,14 +10,31 @@ app.get("/", function(req,res){
 var today = new Date();
 var currentDay = today.getDay();
 var day = "";
-if (currentDay === 6 || currentDay ===0){
 
-day = "weekend";
-}
-else{
-
-day = "weekday";
-}
+switch(currentDay){
+  case 0:
+  day = "Sunday";break;
+case 1:
+  day = "Monday";
+break;
+  case 2:
+  day = "TuesDay";
+  break;
+  case 3:
+  day = "Wednessday";
+  break;
+  case 4:
+  day = "Thursday";
+  break;
+  case 5:
+  day = "Friday";
+  break;
+  case 6:
+    day = "Saturday";
+  break;
+  default:
+  console.log("Errpr: current day is equal to:" + currentDay);
+  }
 res.render("list", {kindOfDay: day});
 
 });
