ef1cfda7b9bd4a526240d53f85ca30530edf31fd
Arpita Naval <navalarpita@gmail.com>
2021-05-27 09:27:59 +0530

    Complete work route

--- app.js
+++ app.js
@@ -7,44 +7,52 @@ workItems = [];
 
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
 
-var today = new Date();
-var currentDay = today.getDay();
-var day = "";
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-var options = {
-  weekday: "long",
-  day: "numeric",
-  month: "long"
-}
-var day = today.toLocaleDateString("en-IN", options);
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+  var day = today.toLocaleDateString("en-IN", options);
 
-res.render("list", {listTitle: day, newListItems : items});
-
-});
-
-app.post("/", function(req,res){
-var item = req.body.userInput;
-items.push(item);
-res.redirect("/");
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
 
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-
-
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work",
+    newListItems: workItems
+  });
 });
-app.post("/work", function(req,res){
-    var item = req.body.userInput;
+
+app.post("/", function(req, res) {
+  var item = req.body.userInput;
+  console.log(req.body.list);
+  if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
-})
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
 
 app.listen(3000, function() {
   console.log(`Server running on port 3000: http://localhost:${3000} `);
