61d466148b1123dda272df66f81564ef7e40c7db
Arpita Naval <navalarpita@gmail.com>
2021-05-27 15:10:21 +0530

    connect with mongoose

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname +"/date.js");
 
 const app = express();
 
-const items = [];
-const workItems = [];
+// const items = [];
+// const workItems = [];
 
 
 app.set("view engine", "ejs");
@@ -13,12 +14,36 @@ app.use(bodyParser.urlencoded({  extended: true }));
 app.use(express.static("public"));
 
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true} );
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({ name: "A" });
+const item2 = new Item({ name: "B" });
+const item3 = new Item({ name: "C" });
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err){
+  if (err) {
+    console.log(err);
+  }else {
+    console.log("Successfully saved default items!");
+  }
+});
+
 app.get("/", function(req, res) {
 
 let day = date.getDate();
+Item.find({}, function(err,foundItems){
+  res.render("list", {listTitle: day, newListItems: foundItems  });
 
-  res.render("list", {listTitle: day, newListItems: items  });
 
+});
 });
 
 app.get("/work", function(req, res) {
