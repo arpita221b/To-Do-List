b4ce4500aef2d93b408c6e4289f029f96d52a532
Arpita Naval <navalarpita@gmail.com>
2021-05-28 17:08:02 +0530

    Connect with mongoose

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname +"/date.js");
+const mongoose = require('mongoose');
 
 const app = express();
 
-const items = [];
-const workItems = [];
-
-
+// const items = [];
+// const workItems = [];
+//
+mongoose.connect("mongodb://localhost:27017/newListDB", {useNewUrlParser: true, useUnifiedTopology: true})
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({  extended: true }));
 app.use(express.static("public"));
 
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+// const item1 = new Item({  name: "A" });
+const items = [];
+
+Item.insertMany(items, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully savevd default items to DB.");
+  }
+});
 
 app.get("/", function(req, res) {
 
