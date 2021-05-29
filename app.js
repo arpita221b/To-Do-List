ec90ed978c5a9bb42fb974dec34cec9c208c92d2
Arpita Naval <navalarpita@gmail.com>
2021-05-29 13:58:57 +0530

    update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 
-mongoose.connect("mongodb+srv://admin-arpita:test123@cluster0.z6rig.mongodb.net/newListDB", {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.URL, {useNewUrlParser: true, useUnifiedTopology: true})
 app.set("view engine", "ejs");
 mongoose.set('useFindAndModify', false);
 
